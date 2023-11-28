59b83f6c861d4336d15ec37cca7f1bf98aa0d466
Marcus Ottoni <marcusvottoni@gmail.com>
2023-11-28 11:14:37 -0300

    primeiro commit projeto react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import './Header.js';
+
+
+function App() {
+  return (
+    <>
+     <Header />;
+    </>
+  );
+}
+
+/* aqui poderia ser crida uma função para ser chamada dentro de outra - exp APP*/
+
+export default App;
